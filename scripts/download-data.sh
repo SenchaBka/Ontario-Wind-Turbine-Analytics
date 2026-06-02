7fa46b4cd2e264611614c6f355bd2f93598f7246
Arsenii Buriak <128735775+SenchaBka@users.noreply.github.com>
2026-06-02 15:03:49 -0400

    Script to download API data from AWS S3 added

---
+++ scripts/download-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Downloading large data files from S3..."
+
+mkdir -p analysis/api
+
+aws s3 sync s3://ontario-wind-turbine-analytics-bucket/analysis-api/ analysis/api/
+
+echo "Download complete"
